279058f0e6d46cb9033083d18c21aae682435d39
Taras Vaskiv <>
2020-09-16 18:58:56 +0300

    Init serverless step functions

---
+++ handler.py
@@ -0,0 +1,49 @@
+import json
+import boto3
+import os
+
+
+from random import randint, choice
+from datetime import datetime
+
+course = (27, 28)
+
+goods = ['book', 'bike', 'car']
+
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+
+def check_stock_value(event, context):
+    body = {
+        "stock_price": randint(0, 100),
+        "name": choice(goods),
+        "datetime": datetime.now().strftime("%d-%m-%Y"),
+    }
+    table = dynamodb.Table(os.environ.get('DYNAMODB_NAME', 'stockTable'))
+    table.put_item(Item=body)
+    print(body)
+    return body
+
+
+def sell(event, context):
+    print(event)
+    body = {
+        "message": "Inside sell",
+        "input": event,
+        "stock_number": randint(0, 100)
+    }
+    print(body)
+    return body
+
+
+def buy(event, context):
+    print(event)
+    body = {
+        "message": "Inside buy",
+        "input": event,
+        "stock_number": randint(0, 100)
+    }
+    print(body)
+    return body
